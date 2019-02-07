60f6e65fde360bcab4a792f95062ad775cd5dc52
Taxin2012 <mrtaxin2012@mail.ru>
2019-02-07 11:56:02 +0300

    Update test.js

--- test.js
+++ test.js
@@ -5,17 +5,17 @@ var newArr = [];
 
 function IsHaveNum( num ) {
 	newArr.forEach(function(num2) {
-    	if num2 == num return true;
-    }
-    return false;
+		if num2 == num return true;
+	}
+	return false;
 }
 
 function FindNum() {
 	for ( let i = 0; i < linesArray[0]; i++ ) {
-    	numbsArr.forEach(function(num2) {
-        	let num = num2 % i;
-        	if ( !IsHaveNum( num ) ) newArr.push( num ) else return i;
-    	}
+		numbsArr.forEach(function(num2) {
+			let num = num2 % i;
+			if ( !IsHaveNum( num ) ) newArr.push( num ) else return i;
+		}
 	}
 }
                      
